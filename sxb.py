8d7c12b1c5f63b3f94472523822e9e953fb9d6f0
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 17:18:28 +0500

    Update sxb.py

--- sxb.py
+++ sxb.py
@@ -1,8 +1,8 @@
 import platform
 bit=platform.architecture()[0]
 if bit =='64bit':
-    import Junaid
-    Junaid._f_a_md__eck()
+    import SXB
+    SXB._f_a_md__eck()
 else:
     print('Sorry device or system not support this tools')
     exit()
