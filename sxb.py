440e04c5c4cc9ac1e1de190fcc6416b99c371c3a
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 18:07:17 +0500

    Update sxb.py

--- sxb.py
+++ sxb.py
@@ -1,8 +1,8 @@
 import platform
 bit=platform.architecture()[0]
 if bit =='64bit':
-    import sxb
-   sxb.subscription
+    import sxb.py
+   sxb.py.subscription
 else:
     print('Sorry device or system not support this tools')
     exit()
