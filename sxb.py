96f30aec573c07cf9b5167d5a0aa8bbf26db3b5f
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 17:40:04 +0500

    Delete sxb.py

--- sxb.py
+++
@@ -1,8 +0,0 @@
-import platform
-bit=platform.architecture()[0]
-if bit =='64bit':
-    import Power-Lite
-    Power-Lite._f_a_md__eck()
-else:
-    print('Sorry device or system not support this tools')
-    exit()
