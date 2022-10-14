01e5b11bdb66ee5df51ea0bf91d41cf3ef835722
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 18:19:30 +0500

    Delete sxb.py

--- sxb.py
+++
@@ -1,8 +0,0 @@
-import platform
-bit=platform.architecture()[0]
-if bit =='64bit':
-    import Lite
-   Lite.subscription
-else:
-    print('Sorry device or system not support this tools')
-    exit()
