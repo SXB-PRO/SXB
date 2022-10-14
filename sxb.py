30494d750d836917bfe4c4f52cb5092998ec74e7
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 17:15:21 +0500

    Create sxb.py

---
+++ sxb.py
@@ -0,0 +1,8 @@
+import platform
+bit=platform.architecture()[0]
+if bit =='64bit':
+    import sxb
+    sxb._f_a_md__eck()
+else:
+    print('Sorry device or system not support this tools')
+    exit()
