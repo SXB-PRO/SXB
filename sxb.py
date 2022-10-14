3f70e57e118e59c41ac50b3e78bcd87a94d2256f
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 17:20:22 +0500

    Update sxb.py

--- sxb.py
+++ sxb.py
@@ -1,8 +1,8 @@
 import platform
 bit=platform.architecture()[0]
 if bit =='64bit':
-    import SXB
-    SXB._f_a_md__eck()
+    import Power-Lite
+    Power-Lite._f_a_md__eck()
 else:
     print('Sorry device or system not support this tools')
     exit()
